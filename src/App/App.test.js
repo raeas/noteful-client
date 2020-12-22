d533785a60829fb620f5109ceb79438170bf2837
Rae Schneider <rannschneid@gmail.com>
2020-12-21 18:40:58 -0700

    test updated

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
-} from '@fortawesome/free-solid-svg-icons'
 import App from './App'
-
-library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
+import { BrowserRouter} from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  )
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
